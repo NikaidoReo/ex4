f2137c814183d93e31a35f8a386d1c89594e9396
s1270058 <s1270058@std6dc46.u-aizu.ac.jp>
2020-08-06 14:32:20 +0900

    User name added

--- HoT.c
+++ HoT.c
@@ -3,37 +3,38 @@
 #include<time.h>
 
 int main(){
+  
     int i=0,j=0;
     char name[10];
     printf("Who are you?\n")0;
     scanf("%s",name);
     printf("Hello!%s\n",name);
     srand(time(NULL));
-    printf("Tossing a coin...¥n");
+    printf("Tossing a coin...促n");
     printf("round1:");
     if(rand()%2==0){
         i++;
-        printf("Heads¥n");
+        printf("Heads促n");
     } else {
         j++;
-        printf("Tails¥n");
+        printf("Tails促n");
     }
     printf("round2:");
     if(rand()%2==0){
         i++;
-        printf("Heads¥n");
+        printf("Heads促n");
     } else {
         j++;
-        printf("Tails¥n");
+        printf("Tails促n");
     }
     printf("round3:");
     if(rand()%2==0){
         i++;
-        printf("Heads¥n");
+        printf("Heads促n");
     } else {
         j++;
-        printf("Tails¥n");
+        printf("Tails促n");
     }
-    printf("Head: %d, Tails: %d¥n",i,j);
+    printf("Head: %d, Tails: %d促n",i,j);
     return 0;
 }
